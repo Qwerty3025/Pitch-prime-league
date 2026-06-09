9c8c3c4fc645367c5e742f4e053a439943b97b71
Haleem Akintayo <127564674+haleemakintayo@users.noreply.github.com>
2026-06-09 23:53:05 +0100

    Refactor navbar in base.html for improved responsiveness and styling; remove unused country field from League model

--- league/models.py
+++ league/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class League(models.Model):
     name = models.CharField(max_length=150)
-    country = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
